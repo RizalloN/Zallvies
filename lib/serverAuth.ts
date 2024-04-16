574b23f5e9148f906c656b560070161aca0b8d42
REVISION-PC\ASUS <uzumakirizallO@gmail.com>
2024-04-16 16:11:44 +0700

    rollback

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -2,11 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
 import prismadb from "@/lib/prismadb";
-<<<<<<< HEAD
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-=======
-import { authOptions } from "@/pages/api/auth/[...nextauth]/route";
->>>>>>> 5cbd41ec33b61fb5b613d3cdb7e25fb6b774ab2f
 
 const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req,res,authOptions);
